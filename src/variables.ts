dc9f8868d1f3516abe158bc5a695263d098d0710
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-12-08 13:43:58 +0800

    perf: change default server URL

--- src/variables.ts
+++ src/variables.ts
@@ -1,9 +1,6 @@
 
-// export const WEBURL: string = 'https://bugana.eidoriantan.me';
-// export const WEBAPI: string = 'https://bugana.eidoriantan.me/api';
-
-export const WEBURL: string = 'http://bugana';
-export const WEBAPI: string = 'http://bugana/api';
+export const WEBURL: string = 'https://bugana.eidoriantan.me';
+export const WEBAPI: string = 'https://bugana.eidoriantan.me/api';
 
 export const MONTHS: string[] = [
   'JANUARY',
